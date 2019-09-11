a74f3e4737bb1cda241813334d56aab0a52dc268
Priyavrat Shukla <priyavrat.shukla@bell.ca>
2019-09-11 14:35:38 +0530

    Updates

---
+++ precache-manifest.77e22b165f7ec5bb309c76b18e52d381.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "340a140dc48e109e29a4",
+    "url": "/meme-generator/static/js/runtime~main.340a140d.js"
+  },
+  {
+    "revision": "65dc64e1e3092dd6de6b",
+    "url": "/meme-generator/static/js/main.65dc64e1.chunk.js"
+  },
+  {
+    "revision": "aa32ba7d4b6524ac13d0",
+    "url": "/meme-generator/static/js/1.aa32ba7d.chunk.js"
+  },
+  {
+    "revision": "65dc64e1e3092dd6de6b",
+    "url": "/meme-generator/static/css/main.f1ddccdc.chunk.css"
+  },
+  {
+    "revision": "0df42f70b1d9f2946401035fbf844b54",
+    "url": "/meme-generator/index.html"
+  }
+];
